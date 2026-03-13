25a84286456d656df38ddeb6ef3febea42fddca5
Dragon Chan <dragon.chan@kellogg.northwestern.edu>
2026-03-12 22:22:19 -0500

    completed main rubric items.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,21 +2,48 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
+
+    # alternative responses to requests other than HTML
+    respond_to do |format|
+      format.html # implicitly renders places/index.html.erb
+      format.json do
+        render :json => @places
+      end
+    end
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    if @current_user
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
+    else
+      @entries = []
+    end
   end
 
   def new
+    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @place = Place.new
+      @place["name"] = params["name"]
+      @place.save
+    else
+      flash["notice"] = "Login first."
+    end
     redirect_to "/places"
+  end
+
+  # removes security restrictions for API calls
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "places", :action => "index" })
   resources "entries"
   resources "places"
   resources "sessions"
   resources "users"
+
+  # Login/logout
+  get("/login", { :controller => "sessions", :action => "new" })
+  get("/logout", { :controller => "sessions", :action => "destroy" })
+
+  # Define the root route
+  get("/", { :controller => "places", :action => "index" })
 end
